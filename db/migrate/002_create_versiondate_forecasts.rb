a8654647625a2559501792339c986189bb4af4da
Moritz Hamann <mohikaner@quantentunnel.de>
2013-06-05 09:58:26 +0200

    changed migration and include patches

--- db/migrate/002_create_versiondate_forecasts.rb
+++ db/migrate/002_create_versiondate_forecasts.rb
@@ -1,6 +1,6 @@
 class CreateVersiondateForecasts < ActiveRecord::Migration
   def change
-    create_table :version_forecasts do |t|
+    create_table :versiondate_forecasts do |t|
       t.integer :version_id
       t.date :forecast_date
       t.date :planned_date
--- lib/patches/ProjectInclude.rb
+++ lib/patches/ProjectInclude.rb
@@ -4,14 +4,26 @@ module ProjectIncludeBudgetReporting
     base.class_eval do 
       has_many :IndividualItem, :dependent => :destroy
       has_many :PlannedBudget, :dependent => :destroy
-      has_many :VersiondateForecast, :dependent => :destroy
       has_many :ProjectbudgetForecast, :dependent => :destroy
     end
   end
 
+end
 
+module VersionIncludeForecastDate
+  
+  def self.included(base)
+    base.class_eval do
+      has_many :VersiondateForecast, :dependent => :destroy
+    end
+  end
+  
 end
 
 unless Project.included_modules.include? ProjectIncludeBudgetReporting
   Project.send(:include, ProjectIncludeBudgetReporting)
+end
+
+unless Version.included_modules.include? VersionIncludeForecastDate
+  Version.send(:include, VersionIncludeForecastDate)
 end
